89d2a940e92be708b9e1e4995b58ad6c6fa54c43
Alexander <pchp250g8@tutamail.com>
2026-02-16 15:24:54 +0300

    Created Project BinToDec

---
+++ BinToDec.pas
@@ -0,0 +1,32 @@
+﻿uses
+  System,
+  System.Text.RegularExpressions;
+begin
+  var nDecNum := 0;
+	var nBinPower := 1;
+	var nMaxBinaryLen := Math.Truncate(Math.Log(Cardinal.MaxValue, 2));
+	var oRegEx := new Regex('[0-1]');
+	Console.WriteLine('Input a binary number');
+	var strLine := Console.ReadLine();
+	var nStrLen := strLine.Length;
+	var nMatches := oRegEx.Matches(strLine).Count;
+	var bRightString := (nStrLen <= nMaxBinaryLen) and (nStrLen = nMatches);
+	if not bRightString then
+  begin
+		Console.WriteLine('Wrong binary number format!!!');
+		Console.Read();
+		exit();
+	end;
+	for var i : integer := 1 to nStrLen do
+  begin
+		var nBinDight : integer := ord(strLine[nStrLen + 1 - i]) - ord('0');
+		nDecNum += (nBinDight * nBinPower);
+		nBinPower *= 2;
+	end;
+	WriteLn
+	(
+	  'The decenary equivalent of the binary number ', strLine, ' is: ',
+	  nDecNum
+	);
+	Console.Read();
+end.
