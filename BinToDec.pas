6055065bf006525c92ef8dc7181cbb2ab03c22aa
Alexander <pchp250g8@tutamail.com>
2026-04-07 14:53:44 +0300

    Modified Source Code

--- BinToDec.pas
+++ BinToDec.pas
@@ -1,16 +1,18 @@
 ﻿uses
   System,
   System.Text.RegularExpressions;
+const 
+  MAX_INT = Cardinal.MaxValue;
 begin
   var nDecNum := 0;
 	var nBinPower := 1;
-	var nMaxBinaryLen := Math.Truncate(Math.Log(Cardinal.MaxValue, 2));
-	var oRegEx := new Regex('[0-1]');
+	var nMaxBinaryLen := Math.Truncate(Math.Log(MAX_INT, 2));
+	var oRegEx := new Regex('^[0-1]+$');
 	Console.WriteLine('Input a binary number');
 	var strLine := Console.ReadLine();
 	var nStrLen := strLine.Length;
-	var nMatches := oRegEx.Matches(strLine).Count;
-	var bRightString := (nStrLen <= nMaxBinaryLen) and (nStrLen = nMatches);
+	var bIsMatch := oRegEx.IsMatch(strLine);
+	var bRightString := (nStrLen <= nMaxBinaryLen) and bIsMatch;
 	if not bRightString then
   begin
 		Console.WriteLine('Wrong binary number format!!!');
